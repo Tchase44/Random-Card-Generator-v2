fce5b2e08388e6618e5d296c67518d97424e1c45
Tchase44 <tchase4544@gmail.com>
2016-12-13 10:17:20 -0600

    shuffle
    found an effecive function to shuffle cards

--- deckBuilding.js
+++ deckBuilding.js
@@ -1,27 +1,11 @@
 var theDeck=[];
+var shuffDeck=[];
 var suits = ['♥','♠','♣','♦'];
 var faces = ["A",'2','Q','3','4','J','5','6','7','8','K','9','10'];
-function randomFace(){
-	return Math.floor(Math.random()*faces.length)
-};
-function randomSuit(){
-	return Math.floor(Math.random()*suits.length)
-};
-function randomPair(){
-    var no1= randomFace();
-    var no2= randomSuit();
-    var mySuit = suits[no2];
-    var myFace = faces[no1];
-  };
+
   function randomCard(){
   	return Math.floor(Math.random()*theDeck.length);
   };
- function randomDeckDeal(){
- 	var a = randomCard();
- 	var face = theDeck[a][0];
-    var suit = theDeck[a][1];
- }
-
 function createDeck(){
 	for (i = 0; i < faces.length; i++) {
     	theDeck.push([faces[i],suits[0]]);
@@ -30,7 +14,20 @@ function createDeck(){
     	theDeck.push([faces[i],suits[3]]);
 	}
 };
-var shuffDeck=[];
+createDeck();
+// function to suffles the deck order. changes original array.
+function shuffleDeck(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+};
 
-
-     
+function randomDeckDeal(){
+ 	var a = randomCard();
+ 	var face = theDeck[a][0]; 	
+    var suit = theDeck[a][1];
+};
